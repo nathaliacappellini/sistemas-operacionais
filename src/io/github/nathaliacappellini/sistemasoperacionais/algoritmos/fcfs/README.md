292f613dd9f81f806f976f99f16bc8c37316fbac
Nathalia Cappellini <nathalias2cappellini@gmail.com>
2025-03-24 23:04:25 -0300

    first commit

---
+++ src/io/github/nathaliacappellini/sistemasoperacionais/algoritmos/fcfs/README.md
@@ -0,0 +1,39 @@
+# Escalonador FCFS (First Come, First Served)
+
+Esta pasta contém uma implementação do algoritmo de escalonamento de processos **FCFS (First Come, First Served)** em Java.
+
+## Como funciona?
+
+O **FCFS** escalona os processos na ordem em que chegam, sem preempção. O primeiro processo que chega é executado até o fim antes de passar para o próximo.
+
+## Estrutura do Projeto
+
+``` bash
+/FCFS
+  │── FCFS.java # Implementação do escalonador FCFS
+  │── Processo.java # Classe que representa um processo
+  │── Main.java # Classe principal que executa o escalonador
+  │── README.md # Documentação do projeto
+```
+
+## Como Executar?
+
+1. ##### Compilar
+
+  ```bash
+  javac AlgoritmosEscalonamento/FCFS/*.java
+  ```
+
+2. ##### Executar
+
+  ```bash
+  java AlgoritmosEscalonamento.FCFS.Main
+  ```
+
+## Referências
+
+- Tanenbaum, A. S. "Modern Operating Systems".
+
+---
+
+<div align="center">Feito por <a href="https://github.com/nathaliacappellini">Nathi</a>.</div>
---
+++ src/io/github/nathaliacappellini/sistemasoperacionais/algoritmos/sjf/SJF.java
@@ -0,0 +1,38 @@
+package io.github.nathaliacappellini.sistemasoperacionais.algoritmos.sjf;
+
+public class SJF {
+    
+    public static void exibirCabecalhoChegada() {
+        System.out.println("---------------------------------------------");
+        System.out.println("Ordem de Chegada de Processos");
+        System.out.println("---------------------------------------------");
+        System.out.println("ID | Chegada | Surto");
+    }
+    
+    public static void ordenarProcessos(Processo p1, Processo p2, Processo p3) {
+        // Cria array com os processos
+        Processo[] processos = {p1, p2, p3};
+        
+        // Ordenação 
+        for (int i = 0; i < processos.length; i++) {
+            for (int j = i + 1; j < processos.length; j++) {
+                if (processos[i].getSurto() > processos[j].getSurto()) {
+                    Processo temp = processos[i];
+                    processos[i] = processos[j];
+                    processos[j] = temp;
+                }
+            }
+        }
+        
+        // Exibe cabeçalho
+        System.out.println("\n---------------------------------------------");
+        System.out.println("Processos ordenados em execução - SJF");
+        System.out.println("---------------------------------------------");
+        System.out.println("ID | Chegada | Surto");
+        
+        // Exibe processos ordenados
+        for (int i = 0; i < processos.length; i++) {
+            processos[i].exibirInfo();
+        }
+    }
+}
